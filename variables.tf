fb00d2ae4c77fafcc463c561bf9f545446158647
Matteo <tambonematteo@gmail.com>
2026-03-24 17:28:46 +0100

    update

--- variables.tf
+++ variables.tf
@@ -1,137 +1,50 @@
-
-variable "project" {
-  description = "GCP project ID"
-  type        = string
-}
+#############################
+#  Variabili principali
+#############################
 
 variable "name" {
-  description = "Instance name"
+  description = "Name of the Cloud SQL instance. If null, Terraform generates a random name."
   type        = string
+  default     = null
+}
+
+variable "project" {
+  description = "Project ID for the Cloud SQL instance."
+  type        = string
+  default     = null
 }
 
 variable "region" {
-  description = "Region"
+  description = "Region where the SQL instance will run."
   type        = string
+  default     = null
 }
 
 variable "database_version" {
-  description = "Database version"
+  description = "Database engine version (MySQL, PostgreSQL, SQL Server)."
   type        = string
 }
 
 variable "deletion_protection" {
-  description = "Deletion protection"
+  description = "Prevents Terraform from accidentally destroying the instance."
   type        = bool
   default     = false
 }
 
-variable "labels" {
-  type    = map(string)
-  default = {}
-}
-
-variable "settings_version" {
-  description = "Settings version"
-  type        = number
-  default     = 2
-}
-
-variable "settings" {
-  type = object({
-    tier                             = string
-    edition                          = optional(string)
-    user_labels                      = optional(map(string), {})
-    auto_upgrade_enabled             = optional(bool)
-    activation_policy                = optional(string)
-    availability_type                = optional(string)
-    collation                        = optional(string)
-    connector_enforcement            = optional(string)
-    data_api_access                  = optional(string)
-    deletion_protection_enabled      = optional(bool)
-    enable_google_ml_integration     = optional(bool)
-    enable_dataplex_integration      = optional(bool)
-    disk_autoresize                  = optional(bool)
-    disk_autoresize_limit            = optional(number)
-    disk_size                        = optional(number)
-    disk_type                        = optional(string)
-    data_disk_provisioned_iops       = optional(string)
-    data_disk_provisioned_throughput = optional(string)
-    pricing_plan                     = optional(string)
-    time_zone                        = optional(string)
-    retain_backups_on_delete         = optional(string)
-  })
-  default = null
-}
-
-variable "maintenance_version" {
-    description = "maintenance_version"
+variable "database_name" {
   type    = string
-  default = null
+  default = ""
 }
 
-variable "master_instance_name" {
-    description = "master_instance_name"
+variable "user_name" {
   type    = string
-  default = null
+  default = ""
 }
 
-variable "replica_names" {
-    description = "replica_names"
-  type    = list(string)
-  default = []
-}
-
-variable "root_password" {
-    description = "root_password"
-  type    = string
-  default = null
-}
-
-variable "root_password_wo" {
-    description = "root_password_wo"
-  type    = string
-  default = null
-}
-
-variable "root_password_wo_version" {
-    description = "root_password_wo_version"
-  type    = string
-  default = null
-}
-
-variable "encryption_key_name" {
-    description = "encryption_key_name"
-  type    = string
-  default = null
-}
-
-variable "final_backup_description" {
-    description = "final_backup_description"
-  type    = bool
-  default = true
-}
-
-variable "clone" {
-  description = "Optional clone configuration block for Cloud SQL instance."
-  type = object({
-    source_instance_name          = string
-    source_project                = optional(string)
-    database_names                = optional(list(string))
-    allocated_ip_range            = optional(string)
-    source_instance_deletion_time = optional(string)
-    restore_backup_context = optional(object({
-      backup_run_id = number
-      instance_id   = optional(string)
-      project       = optional(string)
-    }))
-    replication_cluster = optional(object({
-      psa_write_endpoint       = optional(string)
-      failover_dr_replica_name = optional(string)
-      dr_replica               = optional(bool)
-    }))
-  })
-
-  default = null
+variable "user_password" {
+  type      = string
+  sensitive = true
+  default   = ""
 }
 
 variable "tier" {
@@ -139,24 +52,9 @@ variable "tier" {
   default = "db-f1-micro"
 }
 
-variable "activation_policy" {
-  type    = string
-  default = "ALWAYS"
-}
-
 variable "availability_type" {
   type    = string
   default = "ZONAL"
-}
-
-variable "pricing_plan" {
-  type    = string
-  default = "PER_USE"
-}
-
-variable "auto_upgrade_indexes" {
-  type    = bool
-  default = false
 }
 
 variable "disk_autoresize" {
@@ -174,41 +72,6 @@ variable "disk_type" {
   default = "PD_SSD"
 }
 
-variable "data_cache_enabled" {
-  type    = bool
-  default = false
-}
-
-variable "data_cache_size_gb" {
-  type    = number
-  default = 0.1
-}
-
-variable "maintenance_window_day" {
-  type    = number
-  default = 7
-}
-
-variable "maintenance_window_hour" {
-  type    = number
-  default = 3
-}
-
-variable "maintenance_window_update_track" {
-  type    = string
-  default = "stable"
-}
-
-variable "location_preference_zone" {
-  type    = string
-  default = "a"
-}
-
-variable "location_preference_follow_gce_zone" {
-  type    = bool
-  default = false
-}
-
 variable "backup_configuration_enabled" {
   type    = bool
   default = true
@@ -219,11 +82,6 @@ variable "backup_configuration_binary_log_enabled" {
   default = false
 }
 
-variable "backup_configuration_retention_days" {
-  type    = number
-  default = 7
-}
-
 variable "backup_configuration_point_in_time_recovery_enabled" {
   type    = bool
   default = false
@@ -232,26 +90,6 @@ variable "backup_configuration_point_in_time_recovery_enabled" {
 variable "backup_configuration_start_time" {
   type    = string
   default = "01:00"
-}
-
-variable "backup_configuration_location" {
-  type    = string
-  default = null
-}
-
-variable "backup_configuration_kind" {
-  type    = string
-  default = null
-}
-
-variable "ip_configuration_ipv4_enabled" {
-  type    = bool
-  default = false
-}
-
-variable "ip_configuration_require_ssl" {
-  type    = bool
-  default = true
 }
 
 variable "authorized_networks" {
@@ -270,20 +108,203 @@ variable "database_flags" {
   default = []
 }
 
+variable "settings" {
+  description = "Settings for the Cloud SQL instance."
+  type = object({
+    tier                            = string
+    edition                         = optional(string)
+    user_labels                     = optional(map(string))
+    auto_upgrade_enabled            = optional(bool)
+    activation_policy               = optional(string)
+    availability_type               = optional(string)
+    collation                       = optional(string)
+    connector_enforcement           = optional(string)
+    data_api_access                 = optional(string)
+    deletion_protection_enabled     = optional(bool)
+    enable_google_ml_integration    = optional(bool)
+    enable_dataplex_integration     = optional(bool)
+    disk_autoresize                 = optional(bool)
+    disk_autoresize_limit           = optional(number)
+    disk_size                       = optional(number)
+    disk_type                       = optional(string)
+    data_disk_provisioned_iops      = optional(number)
+    data_disk_provisioned_throughput= optional(number)
+    node_count                      = optional(number)
+    pricing_plan                    = optional(string)
+    time_zone                       = optional(string)
+    retain_backups_on_delete        = optional(bool)
 
-variable "database_name" {
-  type    = string
-  default = ""
+    # Sub-blocks
+    final_backup_config = optional(object({
+      enabled         = optional(bool)
+      retention_days  = optional(number)
+    }))
+
+    advanced_machine_features = optional(object({
+      threads_per_core = optional(number)
+    }))
+
+    database_flags = optional(list(object({
+      name  = string
+      value = string
+    })))
+
+    active_directory_config = optional(object({
+      domain = string
+    }))
+
+    data_cache_config = optional(object({
+      data_cache_enabled = optional(bool)
+    }))
+
+    deny_maintenance_period = optional(list(object({
+      start_date = string
+      end_date   = string
+      time       = string
+    })))
+
+    sql_server_audit_config = optional(object({
+      bucket             = optional(string)
+      upload_interval    = optional(string)
+      retention_interval = optional(string)
+    }))
+
+    backup_configuration = optional(object({
+      binary_log_enabled               = optional(bool)
+      enabled                          = optional(bool)
+      start_time                       = optional(string)
+      point_in_time_recovery_enabled  = optional(bool)
+      location                         = optional(string)
+      transaction_log_retention_days   = optional(number)
+
+      backup_retention_settings = optional(object({
+        retained_backups = optional(number)
+        retention_unit   = optional(string)
+      }))
+    }))
+
+    ip_configuration = optional(object({
+      ipv4_enabled                           = optional(bool)
+      private_network                        = optional(string)
+      ssl_mode                                = optional(string)
+      server_ca_mode                          = optional(string)
+      server_ca_pool                          = optional(string)
+      custom_subject_alternative_names        = optional(list(string))
+      allocated_ip_range                      = optional(string)
+      enable_private_path_for_google_services = optional(bool)
+
+      authorized_networks = optional(list(object({
+        name            = optional(string)
+        value           = string
+        expiration_time = optional(string)
+      })))
+
+      psc_config = optional(list(object({
+        psc_enabled               = optional(bool)
+        allowed_consumer_projects = optional(list(string))
+      })))
+    }))
+
+    location_preference = optional(object({
+      follow_gae_application = optional(string)
+      zone                   = optional(string)
+      secondary_zone         = optional(string)
+    }))
+
+    maintenance_window = optional(object({
+      day          = optional(number)
+      hour         = optional(number)
+      update_track = optional(string)
+    }))
+
+    insights_config = optional(object({
+      query_insights_enabled            = optional(bool)
+      enhanced_query_insights_enabled   = optional(bool)
+      query_string_length               = optional(number)
+      record_application_tags           = optional(bool)
+      record_client_address             = optional(bool)
+      query_plans_per_minute            = optional(number)
+    }))
+
+    password_validation_policy = optional(object({
+      min_length                   = optional(number)
+      complexity                   = optional(string)
+      reuse_interval               = optional(number)
+      disallow_username_substring  = optional(bool)
+      password_change_interval     = optional(number)
+      enable_password_policy       = optional(bool)
+    }))
+  })
+
+  default = null
 }
 
-variable "user_name" {
-  type    = string
-  default = ""
+#############################
+#  CLONE block
+#############################
+
+variable "clone" {
+  type = object({
+    source_instance_name         = string
+    source_project               = optional(string)
+    point_in_time                = optional(string)
+    preferred_zone               = optional(string)
+    database_names               = optional(list(string))
+    allocated_ip_range           = optional(string)
+    source_instance_deletion_time= optional(string)
+  })
+  default = null
 }
 
-variable "user_password" {
-  type      = string
-  sensitive = true
-  default   = ""
+#############################
+# Backup restore context
+#############################
+
+variable "restore_backup_context" {
+  type = object({
+    backup_run_id = number
+    instance_id   = optional(string)
+    project       = optional(string)
+  })
+  default = null
 }
 
+#############################
+# Point-in-time restore
+#############################
+
+variable "point_in_time_restore_context" {
+  type = object({
+    datasource                   = string
+    point_in_time                = string
+    target_instance              = string
+    private_network              = optional(string)
+    preferred_zone               = optional(string)
+    allocated_ip_range           = optional(string)
+    source_instance_deletion_time= optional(string)
+    database_names               = optional(list(string))
+  })
+  default = null
+}
+
+#############################
+# Replica configuration
+#############################
+
+variable "replica_configuration" {
+  type = object({
+    cascadable_replica     = optional(bool)
+    ca_certificate         = optional(string)
+    client_certificate     = optional(string)
+    client_key             = optional(string)
+    connect_retry_interval = optional(number)
+    dump_file_path         = optional(string)
+    failover_target        = optional(bool)
+    master_heartbeat_period= optional(number)
+    password               = optional(string)
+    ssl_cipher             = optional(string)
+    username               = optional(string)
+    verify_server_certificate = optional(bool)
+  })
+  default = null
+}
