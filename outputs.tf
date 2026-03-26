dc36ba079c1a1996fb4dd46233ea3962b348d9c2
Matteo <tambonematteo@gmail.com>
2026-03-26 09:51:02 +0100

    update

--- outputs.tf
+++ outputs.tf
@@ -1,29 +1,29 @@
 output "instance_id" {
-  value = google_sql_database_instance.main.id
+  value = google_sql_database_instance.sql_database_instance.id
 }
 
 output "instance_name" {
-  value = google_sql_database_instance.main.name
+  value = google_sql_database_instance.sql_database_instance.name
 }
 
 output "connection_name" {
-  value = google_sql_database_instance.main.connection_name
+  value = google_sql_database_instance.sql_database_instance.connection_name
 }
 
 output "public_ip_address" {
-  value = google_sql_database_instance.main.public_ip_address
+  value = google_sql_database_instance.sql_database_instance.public_ip_address
 }
 
 output "private_ip_address" {
-  value = google_sql_database_instance.main.private_ip_address
+  value = google_sql_database_instance.sql_database_instance.private_ip_address
 }
 
 output "service_account_email_address" {
-  value = google_sql_database_instance.main.service_account_email_address
+  value = google_sql_database_instance.sql_database_instance.service_account_email_address
 }
 
 output "proxy_connection_string" {
-  value = "projects/${coalesce(var.project, data.google_project.project.project_id)}/instances/${coalesce(var.name, google_sql_database_instance.main.name)}"
+  value = "projects/${coalesce(var.project, data.google_project.project.project_id)}/instances/${coalesce(var.name, google_sql_database_instance.sql_database_instance.name)}"
 }
 
 data "google_project" "project" {}
